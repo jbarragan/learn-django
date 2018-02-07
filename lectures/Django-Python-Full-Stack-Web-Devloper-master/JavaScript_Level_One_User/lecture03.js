62af75509018e9d2296edf673fce257a964202bd
Jaime Barragan <jaime.barragan@gmail.com>
2018-02-07 09:12:33 -0700

    Javacript Lesson03.

---
+++ lectures/Django-Python-Full-Stack-Web-Devloper-master/JavaScript_Level_One_User/lecture03.js
@@ -0,0 +1,4 @@
+var weight_pounds = prompt("What is the weight in pounds (lbs)?");
+var weight_kgs = weight_pounds*0.454;
+alert("That is: " + weight_kgs + " kilograms");
+console.log("Conversion completed.");
