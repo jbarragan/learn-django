88467fed656cb88a904136c32f96a0e19ae96931
Jaime Barragan <jaimebarragan@Jaimes-Air.T-mobile.com>
2018-02-11 05:17:41 -0700

    jQuery Part2.

---
+++ lectures/Django-Python-Full-Stack-Web-Devloper-master/jQuery_User/Part2.js
@@ -0,0 +1,30 @@
+$('h1').click(function(){
+  console.log("There was a click");
+  console.log("There was a click");
+  $(this).text("I was changed!");
+});
+
+$('li').click(function() {
+  console.log('any li was clicked!');
+});
+
+$('input').eq(0).keypress(function() {
+  $('h3').toggleClass('turnBlue');
+  console.log(event);
+  if (event.which === 13) {
+    $('h3').toggleClass('turnRed');
+  }
+});
+
+$('h1').on('dblclick', function() {
+  $(this).toggleClass('turnBlue')
+})
+
+$('h1').on('mouseenter', function() {
+  $(this).toggleClass('turnBlue')
+});
+
+$('input').eq(1).on('click', function() {
+  // $('.container').fadeOut(3000);
+  $('.container').slideUp(3000);
+});
