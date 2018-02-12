9b8fbecd0eb5355005ebd3e7a456b28ecbd346c1
Jaime Barragan <jaime.barragan@gmail.com>
2018-02-12 08:51:58 -0700

    Python Part1.

---
+++ lectures/Django-Python-Full-Stack-Web-Devloper-master/Python_Level_One_User/Part1.py
@@ -0,0 +1,23 @@
+print("Hello world")
+print(2+1)
+print(2-1)
+print(2*1)
+print(2/1)
+print(2**2)
+print(2**(1/2))
+
+print(2+10 * 10 +3)
+print((2+10) * (10 +3))
+
+a = 5
+print(a)
+print(a+a)
+a = a*5
+
+# This is a comment.
+print(a)
+
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes)
