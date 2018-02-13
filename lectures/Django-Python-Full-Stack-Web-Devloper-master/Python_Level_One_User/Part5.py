1020176c527fe28598f2e92bcdfbcab34e41ae52
Jaime Barragan <jaime.barragan@gmail.com>
2018-02-13 07:19:33 -0700

    Python Level One Part5.

---
+++ lectures/Django-Python-Full-Stack-Web-Devloper-master/Python_Level_One_User/Part5.py
@@ -0,0 +1,24 @@
+# Booleans
+True
+False
+
+# Tuples
+t = (1,2,3)
+print(t[0])
+
+t = ('a',True,123)
+print(t)
+
+# Sets
+x = set()
+x.add(1)
+x.add(2)
+x.add(4)
+x.add(0.1)
+x.add(4)
+x.add(4)
+x.add(4)
+print(x)
+
+converted = set([1, 1, 2, 3, 3, 2, 1])
+print(converted)
