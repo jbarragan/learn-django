af1fb14a5fd44377468cbb709b5d32304839ccd3
Jaime Barragan <jaime.barragan@gmail.com>
2018-02-15 09:08:08 -0700

    Python Level Two Part9.

---
+++ lectures/Django-Python-Full-Stack-Web-Devloper-master/Python_Level_Two_User/Part9_two.py
@@ -0,0 +1,10 @@
+import Part9_one as one
+
+print("TOP LEVEL TWO.PY")
+one.func()
+
+
+if __name__ == "__main__":
+    print("two.py is being run directly")
+else:
+    print("two.py has been imported")
