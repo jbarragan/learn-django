d3e983d5881594e59c77cb09d728188e3986bbe9
Jaime Barragan <jaime.barragan@gmail.com>
2018-02-16 15:35:26 -0700

    Add ProTwo template failing.

--- lectures/Django-Python-Full-Stack-Web-Devloper-master/Django_Level_One_User/ProTwo/AppTwo/views.py
+++ lectures/Django-Python-Full-Stack-Web-Devloper-master/Django_Level_One_User/ProTwo/AppTwo/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("<em>My Second Project</em>")
+
+def help(request):
+    context_dir = {"help_text": "This help is comming from Django!!"}
+    return render(request, 'index.html', context=context_dir)
--- lectures/Django-Python-Full-Stack-Web-Devloper-master/Django_Level_One_User/ProTwo/ProTwo/urls.py
+++ lectures/Django-Python-Full-Stack-Web-Devloper-master/Django_Level_One_User/ProTwo/ProTwo/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls import include
 from django.contrib import admin
 from AppTwo import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^help/', include('AppTwo.urls')),
     url(r'^admin/', admin.site.urls),
 ]
