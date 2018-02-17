01d5c4b0085d894bae1acc074680b65c99fa0322
Jaime Barragan <jaimebarragan@Jaimes-MacBook-Air.local>
2018-02-17 04:07:11 -0700

    Django Level One: Add static files.

--- lectures/Django-Python-Full-Stack-Web-Devloper-master/Django_Level_One_User/first_project/first_app/urls.py
+++ lectures/Django-Python-Full-Stack-Web-Devloper-master/Django_Level_One_User/first_project/first_app/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from first_app import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index")
+    url(r'^$', views.index, name="index"),
+    url(r'^picture', views.picture, name="picture")
 ]
--- lectures/Django-Python-Full-Stack-Web-Devloper-master/Django_Level_One_User/first_project/first_app/views.py
+++ lectures/Django-Python-Full-Stack-Web-Devloper-master/Django_Level_One_User/first_project/first_app/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 def index(request):
     my_dict = { 'insert_me': "Hello I am from views.py and my template is comming from first_app/index.html !!" }
     return render(request, 'first_app/index.html', context=my_dict)
+
+def picture(request):
+    return render(request, 'first_app/picture.html', context={})
